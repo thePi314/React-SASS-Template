37403bf4ea63c90ba5725a69167426e24c524a3b
LongLostHero <89987994+LongLostHero@users.noreply.github.com>
2022-03-16 22:49:43 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./screens/login";
 import AdminDashboard from "./screens/admin/dashboard";
+import Nezime from "./screens/nezime/nezime";
+import Navbar from "./components/navbar";
+
+
+
+
 
 
 const App = () => {
     return (
+        <div className="screen">
         <BrowserRouter>
+            <Navbar/>
             <Routes>
+                <Route path="/" element={<Nezime />} />
                 <Route path="/" element={<Login />} />
                 <Route path="/admin/dashboard" element={<AdminDashboard />} />
             </Routes>
         </BrowserRouter>
+        
+    </div>
     );
 };
 
