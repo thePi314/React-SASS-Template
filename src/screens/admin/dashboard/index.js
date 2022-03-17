c7f4d6f1a5a4f37289bc8ee2219aa5ccb31002ba
SilaForce <90404958+SilaForce@users.noreply.github.com>
2022-03-17 09:28:31 +0100

    Add files via upload

--- src/screens/admin/dashboard/index.js
+++ src/screens/admin/dashboard/index.js
@@ -7,6 +7,8 @@ import CandidatesSegment from "./candidates";
 
 import Avatar from "../../../assets/images/avatar.png"
 
+import Table from "../../../components/table/index"
+
 
 const DashboardRoutes = [
     {
@@ -26,23 +28,7 @@ const AdminDashboard = () => {
 
     return (
         <div className="screen admin-dashboard-screen">
-            <AdminDashboardNavbar 
-                routes={DashboardRoutes}
-                setActiveRoute={setActiveRoute}
-                activeRoute={activeRoute}
-            />
-            <div className="navbar-mobile">
-                <AdminDashboardNavbar 
-                    className="content"
-                    routes={DashboardRoutes}
-                    setActiveRoute={setActiveRoute}
-                    activeRoute={activeRoute}
-                />
-            </div>
-
-            <div className="content">
-                {DashboardRoutes[activeRoute].component}
-            </div>
+          <Table />
         </div>
     );
 };
